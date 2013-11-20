a9370704a783e6ccb8112758c5fcfe51fff1855e
Chris Long <chris.long@mac.com>
2013-11-20 13:08:04 -0700

    bumped docker version to 0.6.6

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,7 +7,7 @@ sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.
 apt-get -y update
 
 # Install, you will see another warning that the package cannot be authenticated. Confirm install.
-apt-get -y install lxc-docker-0.6.4
+apt-get -y install lxc-docker-0.6.6
 
 sed -i "s~^.*/usr/bin/docker -d~  /usr/bin/docker -d -r -H 'unix:///var/run/docker.sock' -H 'tcp://0.0.0.0:4243' -api-enable-cors~" /etc/init/docker.conf
 service docker restart
